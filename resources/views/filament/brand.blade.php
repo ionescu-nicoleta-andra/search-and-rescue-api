f4e625f9cc84de8379e7947198d8cdcb06a20058
ionescu-nicoleta-andra <ionescu.nicoleta.andra@gmail.com>
2025-10-21 13:53:02 +0300

    added logo and name in the navbar

--- resources/views/filament/brand.blade.php
+++ resources/views/filament/brand.blade.php
@@ -1,4 +1,4 @@
 <div class="inline-flex items-center gap-2 align-middle">
     <img src="{{ asset('images/logo.png') }}" alt="Logo" class="h-8 inline-block" width="30">
-    <span class="font-semibold text-lg text-gray-800 dark:text-gray-100">Search & Rescue Admin Panel</span>
+    <span class="font-semibold text-lg text-gray-800 dark:text-gray-100">ADCS  Admin Panel</span>
 </div>
